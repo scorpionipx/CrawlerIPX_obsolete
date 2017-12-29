fad8ce94344010d396332057c6dcbb4a9cd46915
DESKTOP-VB5MA2I\ScorpionIPX <scorpionipx@gmail.com>
2017-12-29 16:23:10 +0200

    Created basic tests for host instances

--- test/test_host.py
+++ test/test_host.py
@@ -1,14 +1,22 @@
 import unittest
 
-from crawler_ipx import host
+from crawler_ipx.host import Host
 
 
 class TestHostCreation(unittest.TestCase):
 
-    def test_creation(self):
-        a = 2
-        print(a)
+    def test_host_initialization(self):
+        ipx_host = Host()
+        self.assertIsInstance(ipx_host, Host)
 
+
+class TestHostInformation(unittest.TestCase):
+
+    def test_host_information(self):
+        ipx_host = Host()
+
+        host_info = ipx_host.get_info()
+        print(host_info)
 
 
 
