e7a2e61e38e80a01648f5468c6e2a4525acce41c
DESKTOP-VB5MA2I\ScorpionIPX <scorpionipx@gmail.com>
2017-12-29 10:36:15 +0200

    Added logo image

---
+++ test/run_all.py
@@ -0,0 +1,21 @@
+"""
+    Test function to execute all tests found within current directory
+"""
+import sys
+import xmlrunner
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+def main():
+    tests = unittest.TestLoader().discover('.', 'test_*.py')
+    test_result = xmlrunner.XMLTestRunner(output='test-reports').run(tests)
+
+    return not test_result.wasSuccessful()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
