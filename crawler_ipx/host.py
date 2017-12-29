85691af0871395710974b76ab1cfe2e3b734656a
DESKTOP-VB5MA2I\ScorpionIPX <scorpionipx@gmail.com>
2017-12-29 15:26:30 +0200

    Properly configured logger

---
+++ crawler_ipx/host.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger('ipx_logger')
+
+logger.info('Printed from host')
+
+logger.debug('Debug from host')
+
+
+
---
+++ test/test_host.py
@@ -0,0 +1,14 @@
+import unittest
+
+from crawler_ipx import host
+
+
+class TestHostCreation(unittest.TestCase):
+
+    def test_creation(self):
+        a = 2
+        print(a)
+
+
+
+
